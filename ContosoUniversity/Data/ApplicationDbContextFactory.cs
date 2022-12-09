e8b3e3791ebf66b8edb4c7f8e3d072a5b0e28423
nicu-danci <danci0489@gmail.com>
2022-12-09 08:13:04 +0000

    Add project files.

---
+++ ContosoUniversity/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using ContosoUniversity.Data;
+//using Microsoft.EntityFrameworkCore.Design;
+//using Microsoft.EntityFrameworkCore;
+
+//public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+//{
+//    public ApplicationDbContext CreateDbContext(string[] args)
+//    {
+//        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+//        optionsBuilder.UseSqlServer("Server=YourServer; Database=YourDb; Integrated Security=true; MultipleActiveResultSets=true; Trusted_Connection=True");
+
+//        return new ApplicationDbContext(optionsBuilder.Options);
+//    }
+//}
---
+++ ContosoUniversity/Data/SchoolContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+public class SchoolDbContextFactory : IDesignTimeDbContextFactory<SchoolContext>
+{
+    public SchoolContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<SchoolContext>();
+        optionsBuilder.UseSqlServer("Server=YourServer; Database=YourDb; Integrated Security=true; MultipleActiveResultSets=true; Trusted_Connection=True");
+
+        return new SchoolContext(optionsBuilder.Options);
+    }
+}
